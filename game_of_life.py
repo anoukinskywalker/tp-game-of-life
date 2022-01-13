ee8633343f5faae41d60c2623614e3d275e3e406
Anouk <anoukvangrieken@gmail.com>
2022-01-13 14:03:06 +0100

    retraction fonction inutile

--- game_of_life.py
+++ game_of_life.py
@@ -81,50 +81,11 @@ while not done:
     new_array = copy.deepcopy(initial_state)
 
     for i in range(lines) :
-
-        if(i==0):
-            kmin=0
-        else :
-            kmin=i-1
-                
-        if(i==lines):
-            kmax=lines
-        elif (i==(lines-1)):
-                kmax=i+1
-        else :
-            kmax=i+2
-
         for j in range(columns) :
 
-            if(j==0):
-                lmin=0
-            else :
-                lmin=j-1
-            if(j==columns):
-                lmax=columns
-            elif (j==(columns-1)):
-                lmax=j+1
-            else :
-                lmax=j+2
-            
-            cell_counter = 0
-
-            for k in range(kmin, kmax) :
-                for l in range (lmin, lmax) :
-                    if (initial_state[k][l]==1) :
-                        cell_counter+=1
-
             if (initial_state[i][j]==1) :
-                cell_counter-=1
                 pygame.draw.rect(screen, (255, 255, 255), (50*j, 50*i, 50, 50))
-                if (cell_counter<2 or cell_counter>3):
-                    new_array[i][j]=0
-                else :
-                    new_array[i][j]=1
-            else :
-                if (cell_counter==3):
-                    new_array[i][j]=1
-    initial_state=copy.deepcopy(new_array)
+    initial_state=get_next_state(initial_state)
             
 
     pygame.display.flip()
